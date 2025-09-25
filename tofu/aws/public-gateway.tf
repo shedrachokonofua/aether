e9524dc5008eb1d9ef32d7f3195e9c2e8e4605bf
Shedrach Osemudiamhen Okonofua <shedrachokonofua@gmail.com>
2025-09-24 23:14:14 -0400

    Setup cloudflare dns and proxy for public gateway

--- tofu/aws/public-gateway.tf
+++ tofu/aws/public-gateway.tf
@@ -2,12 +2,34 @@ resource "aws_lightsail_key_pair" "public_gateway_key_pair" {
   name = "aether-public-gateway-key-pair"
 }
 
+resource "local_file" "public_gateway_private_key" {
+  content         = aws_lightsail_key_pair.public_gateway_key_pair.private_key
+  filename        = "../secrets/public_gateway_private_key.pem"
+  file_permission = "0600"
+}
+
 resource "aws_lightsail_instance" "public_gateway" {
   name              = "aether-public-gateway"
   availability_zone = "us-east-1b"
   blueprint_id      = "amazon_linux_2023"
   bundle_id         = "nano_3_0"
   key_pair_name     = aws_lightsail_key_pair.public_gateway_key_pair.id
+}
+
+resource "aws_lightsail_instance_public_ports" "public_gateway_public_ports" {
+  instance_name = aws_lightsail_instance.public_gateway.id
+
+  port_info {
+    protocol  = "tcp"
+    from_port = 22
+    to_port   = 22
+  }
+
+  port_info {
+    protocol  = "tcp"
+    from_port = 443
+    to_port   = 443
+  }
 }
 
 resource "aws_lightsail_static_ip" "public_gateway_static_ip" {
@@ -20,7 +42,7 @@ resource "aws_lightsail_static_ip_attachment" "public_gateway_static_ip_attachme
 }
 
 output "public_gateway_ip" {
-  value = aws_lightsail_static_ip.public_gateway_static_ip.id
+  value = aws_lightsail_static_ip.public_gateway_static_ip.ip_address
 }
 
 output "public_gateway_public_key" {
@@ -30,4 +52,8 @@ output "public_gateway_public_key" {
 output "public_gateway_private_key" {
   value     = aws_lightsail_key_pair.public_gateway_key_pair.private_key
   sensitive = true
+}
+
+output "public_gateway_private_key_path" {
+  value = local_file.public_gateway_private_key.filename
 }
--- tofu/cloudflare.tf
+++ tofu/cloudflare.tf
@@ -10,13 +10,45 @@ resource "cloudflare_zone" "shdrch_domain" {
   }
 }
 
+resource "cloudflare_zone_setting" "shdrch_ssl" {
+  zone_id    = cloudflare_zone.shdrch_domain.id
+  setting_id = "ssl"
+  value      = "strict"
+}
+
+resource "cloudflare_dns_record" "aether_public_gateway_root" {
+  name    = "@"
+  content = module.aws.public_gateway_ip
+  type    = "A"
+  zone_id = cloudflare_zone.shdrch_domain.id
+  proxied = true
+  ttl     = 1
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "cloudflare_dns_record" "aether_public_gateway_wildcard" {
+  name    = "*"
+  content = module.aws.public_gateway_ip
+  type    = "A"
+  zone_id = cloudflare_zone.shdrch_domain.id
+  proxied = true
+  ttl     = 1
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "cloudflare_dns_record" "shdr_ch_dkim_protonmail" {
-  name        = "protonmail._domainkey.shdr.ch"
-  content     = "protonmail.domainkey.doppwpp2flj65ryomxwk7mre2jvwcrl2wvszn5cbvpxo5blaw2yfa.domains.proton.ch"
-  type        = "CNAME"
-  zone_id     = cloudflare_zone.shdrch_domain.id
-  proxied     = false
-  ttl         = 1
+  name    = "protonmail._domainkey.shdr.ch"
+  content = "protonmail.domainkey.doppwpp2flj65ryomxwk7mre2jvwcrl2wvszn5cbvpxo5blaw2yfa.domains.proton.ch"
+  type    = "CNAME"
+  zone_id = cloudflare_zone.shdrch_domain.id
+  proxied = false
+  ttl     = 1
 
   lifecycle {
     prevent_destroy = true
@@ -24,12 +56,12 @@ resource "cloudflare_dns_record" "shdr_ch_dkim_protonmail" {
 }
 
 resource "cloudflare_dns_record" "shdr_ch_dkim_protonmail2" {
-  name        = "protonmail2._domainkey.shdr.ch"
-  content     = "protonmail2.domainkey.doppwpp2flj65ryomxwk7mre2jvwcrl2wvszn5cbvpxo5blaw2yfa.domains.proton.ch"
-  type        = "CNAME"
-  zone_id     = cloudflare_zone.shdrch_domain.id
-  proxied     = false
-  ttl         = 1
+  name    = "protonmail2._domainkey.shdr.ch"
+  content = "protonmail2.domainkey.doppwpp2flj65ryomxwk7mre2jvwcrl2wvszn5cbvpxo5blaw2yfa.domains.proton.ch"
+  type    = "CNAME"
+  zone_id = cloudflare_zone.shdrch_domain.id
+  proxied = false
+  ttl     = 1
 
   lifecycle {
     prevent_destroy = true
@@ -38,12 +70,12 @@ resource "cloudflare_dns_record" "shdr_ch_dkim_protonmail2" {
 
 
 resource "cloudflare_dns_record" "shdr_ch_dkim_protonmail3" {
-  name        = "protonmail3._domainkey.shdr.ch"
-  content     = "protonmail3.domainkey.doppwpp2flj65ryomxwk7mre2jvwcrl2wvszn5cbvpxo5blaw2yfa.domains.proton.ch"
-  type        = "CNAME"
-  zone_id     = cloudflare_zone.shdrch_domain.id
-  proxied     = false
-  ttl         = 1
+  name    = "protonmail3._domainkey.shdr.ch"
+  content = "protonmail3.domainkey.doppwpp2flj65ryomxwk7mre2jvwcrl2wvszn5cbvpxo5blaw2yfa.domains.proton.ch"
+  type    = "CNAME"
+  zone_id = cloudflare_zone.shdrch_domain.id
+  proxied = false
+  ttl     = 1
 
   lifecycle {
     prevent_destroy = true
@@ -51,13 +83,13 @@ resource "cloudflare_dns_record" "shdr_ch_dkim_protonmail3" {
 }
 
 resource "cloudflare_dns_record" "shdr_ch_mx_protonmail_primary" {
-  name        = "shdr.ch"
-  content     = "mail.protonmail.ch"
-  type        = "MX"
-  priority    = 10
-  zone_id     = cloudflare_zone.shdrch_domain.id
-  proxied     = false
-  ttl         = 1
+  name     = "shdr.ch"
+  content  = "mail.protonmail.ch"
+  type     = "MX"
+  priority = 10
+  zone_id  = cloudflare_zone.shdrch_domain.id
+  proxied  = false
+  ttl      = 1
 
   lifecycle {
     prevent_destroy = true
@@ -65,13 +97,13 @@ resource "cloudflare_dns_record" "shdr_ch_mx_protonmail_primary" {
 }
 
 resource "cloudflare_dns_record" "shdr_ch_mx_protonmail_secondary" {
-  name        = "shdr.ch"
-  content     = "mailsec.protonmail.ch"
-  type        = "MX"
-  priority    = 20
-  zone_id     = cloudflare_zone.shdrch_domain.id
-  proxied     = false
-  ttl         = 1
+  name     = "shdr.ch"
+  content  = "mailsec.protonmail.ch"
+  type     = "MX"
+  priority = 20
+  zone_id  = cloudflare_zone.shdrch_domain.id
+  proxied  = false
+  ttl      = 1
 
   lifecycle {
     prevent_destroy = true
@@ -79,12 +111,12 @@ resource "cloudflare_dns_record" "shdr_ch_mx_protonmail_secondary" {
 }
 
 resource "cloudflare_dns_record" "shdr_ch_dmarc" {
-  name        = "_dmarc.shdr.ch"
-  content     = "\"v=DMARC1; p=quarantine; rua=mailto:3468988e99d54c95aacf348d4d3c4542@dmarc-reports.cloudflare.net;\""
-  type        = "TXT"
-  zone_id     = cloudflare_zone.shdrch_domain.id
-  proxied     = false
-  ttl         = 1
+  name    = "_dmarc.shdr.ch"
+  content = "\"v=DMARC1; p=quarantine; rua=mailto:3468988e99d54c95aacf348d4d3c4542@dmarc-reports.cloudflare.net;\""
+  type    = "TXT"
+  zone_id = cloudflare_zone.shdrch_domain.id
+  proxied = false
+  ttl     = 1
 
   lifecycle {
     prevent_destroy = true
@@ -92,12 +124,12 @@ resource "cloudflare_dns_record" "shdr_ch_dmarc" {
 }
 
 resource "cloudflare_dns_record" "shdr_ch_spf" {
-  name        = "shdr.ch"
-  content     = "\"v=spf1 include:_spf.protonmail.ch ~all\""
-  type        = "TXT"
-  zone_id     = cloudflare_zone.shdrch_domain.id
-  proxied     = false
-  ttl         = 1
+  name    = "shdr.ch"
+  content = "\"v=spf1 include:_spf.protonmail.ch ~all\""
+  type    = "TXT"
+  zone_id = cloudflare_zone.shdrch_domain.id
+  proxied = false
+  ttl     = 1
 
   lifecycle {
     prevent_destroy = true
@@ -105,12 +137,12 @@ resource "cloudflare_dns_record" "shdr_ch_spf" {
 }
 
 resource "cloudflare_dns_record" "shdr_ch_protonmail_verification" {
-  name        = "shdr.ch"
-  content     = "\"protonmail-verification=3d35cf0118ffc020071fb50798efae703760474c\""
-  type        = "TXT"
-  zone_id     = cloudflare_zone.shdrch_domain.id
-  proxied     = false
-  ttl         = 1
+  name    = "shdr.ch"
+  content = "\"protonmail-verification=3d35cf0118ffc020071fb50798efae703760474c\""
+  type    = "TXT"
+  zone_id = cloudflare_zone.shdrch_domain.id
+  proxied = false
+  ttl     = 1
 
   lifecycle {
     prevent_destroy = true
