5f7345e07e696cc2323014a220aa0cb3400356d8
Shedrach Okonofua <shedrachokonofua@gmail.com>
2026-04-30 23:20:36 -0400

    Bump public gateway

--- tofu/aws/public-gateway.tf
+++ tofu/aws/public-gateway.tf
@@ -12,7 +12,7 @@ resource "aws_lightsail_instance" "public_gateway" {
   name              = "aether-public-gateway"
   availability_zone = "us-east-1b"
   blueprint_id      = "amazon_linux_2023"
-  bundle_id         = "nano_3_0"
+  bundle_id         = "micro_3_0"
   key_pair_name     = aws_lightsail_key_pair.public_gateway_key_pair.id
 }
 
