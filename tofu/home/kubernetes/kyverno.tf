6ab787a8b6b580df9a913194cb0781be4d80e083
Shedrach Okonofua <shedrachokonofua@gmail.com>
2026-05-13 01:27:38 -0400

    Add DockerHub pull secret policy

--- tofu/home/kubernetes/kyverno.tf
+++ tofu/home/kubernetes/kyverno.tf
@@ -71,6 +71,155 @@ resource "helm_release" "kyverno" {
   })]
 }
 
+locals {
+  dockerhub_pull_secret_name    = "dockerhub-creds"
+  dockerhub_registry_servers    = toset(["https://index.docker.io/v1/", "https://registry-1.docker.io", "registry-1.docker.io", "https://registry.hub.docker.com", "registry.hub.docker.com", "https://docker.io", "docker.io", "docker.getoutline.com"])
+  dockerhub_registry_username   = var.secrets["dockerhub.username"]
+  dockerhub_registry_email      = var.secrets["dockerhub.email"]
+  dockerhub_registry_pat        = var.secrets["dockerhub.pat"]
+  dockerhub_registry_auth_value = base64encode("${local.dockerhub_registry_username}:${local.dockerhub_registry_pat}")
+  dockerhub_registry_auths = {
+    for server in local.dockerhub_registry_servers : server => {
+      username = local.dockerhub_registry_username
+      password = local.dockerhub_registry_pat
+      email    = local.dockerhub_registry_email
+      auth     = local.dockerhub_registry_auth_value
+    }
+  }
+}
+
+resource "kubernetes_secret_v1" "dockerhub_pull_secret_source" {
+  depends_on = [helm_release.kyverno]
+
+  metadata {
+    name      = local.dockerhub_pull_secret_name
+    namespace = helm_release.kyverno.namespace
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = local.dockerhub_registry_auths
+    })
+  }
+}
+
+resource "kubectl_manifest" "kyverno_dockerhub_pull_secret_rbac" {
+  depends_on = [helm_release.kyverno]
+
+  yaml_body = yamlencode({
+    apiVersion = "rbac.authorization.k8s.io/v1"
+    kind       = "ClusterRole"
+    metadata = {
+      name = "kyverno:dockerhub-pull-secret"
+      labels = {
+        "app.kubernetes.io/component"                        = "rbac"
+        "app.kubernetes.io/instance"                         = helm_release.kyverno.name
+        "app.kubernetes.io/part-of"                          = "kyverno"
+        "rbac.kyverno.io/aggregate-to-admission-controller"  = "true"
+        "rbac.kyverno.io/aggregate-to-background-controller" = "true"
+      }
+    }
+    rules = [{
+      apiGroups = [""]
+      resources = ["secrets"]
+      verbs     = ["get", "list", "watch", "create", "update", "patch", "delete"]
+    }]
+  })
+}
+
+resource "kubectl_manifest" "kyverno_dockerhub_pull_secret" {
+  depends_on = [
+    helm_release.kyverno,
+    kubectl_manifest.kyverno_dockerhub_pull_secret_rbac,
+    kubernetes_secret_v1.dockerhub_pull_secret_source,
+  ]
+
+  yaml_body = yamlencode({
+    apiVersion = "kyverno.io/v1"
+    kind       = "ClusterPolicy"
+    metadata = {
+      name = "dockerhub-pull-secret"
+      annotations = {
+        "policies.kyverno.io/title"       = "Docker Hub Pull Secret"
+        "policies.kyverno.io/category"    = "Registry"
+        "policies.kyverno.io/subject"     = "Namespace,ServiceAccount,Pod"
+        "policies.kyverno.io/description" = "Clone the Docker Hub pull secret into namespaces and attach it to new ServiceAccounts and Pods."
+      }
+    }
+    spec = {
+      background = true
+      rules = [
+        {
+          name = "clone-dockerhub-pull-secret"
+          match = {
+            any = [{
+              resources = {
+                kinds = ["Namespace"]
+              }
+            }]
+          }
+          exclude = {
+            any = [{
+              resources = {
+                names = [helm_release.kyverno.namespace]
+              }
+            }]
+          }
+          generate = {
+            synchronize = true
+            apiVersion  = "v1"
+            kind        = "Secret"
+            name        = local.dockerhub_pull_secret_name
+            namespace   = "{{ request.object.metadata.name }}"
+            clone = {
+              namespace = helm_release.kyverno.namespace
+              name      = local.dockerhub_pull_secret_name
+            }
+          }
+        },
+        {
+          name = "add-dockerhub-pull-secret-to-serviceaccounts"
+          match = {
+            any = [{
+              resources = {
+                kinds = ["ServiceAccount"]
+              }
+            }]
+          }
+          mutate = {
+            patchStrategicMerge = {
+              imagePullSecrets = [{
+                name = local.dockerhub_pull_secret_name
+              }]
+            }
+          }
+        },
+        {
+          name = "add-dockerhub-pull-secret-to-pods"
+          match = {
+            any = [{
+              resources = {
+                kinds = ["Pod"]
+              }
+            }]
+          }
+          mutate = {
+            patchStrategicMerge = {
+              spec = {
+                imagePullSecrets = [{
+                  name = local.dockerhub_pull_secret_name
+                }]
+              }
+            }
+          }
+        },
+      ]
+    }
+  })
+}
+
 resource "kubectl_manifest" "kyverno_arm_pool_guardrails" {
   depends_on = [helm_release.kyverno]
 
