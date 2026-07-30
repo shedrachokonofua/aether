8eb254fcdfeb8b192c6b98becdb8ef3aefc90282
Shedrach Okonofua <shedrachokonofua@gmail.com>
2026-07-30 07:46:27 -0400

    chore(deskplane): panel separation at bdbf7558

--- tofu/home/kubernetes/deskplane.tf
+++ tofu/home/kubernetes/deskplane.tf
@@ -10,8 +10,8 @@ locals {
   deskplane_namespace      = "deskplane"
   deskplane_host           = "desktop.home.shdr.ch"
   deskplane_public_url     = "https://${local.deskplane_host}"
-  deskplane_chart_version  = "0.1.0-cbfe9ccd"
-  deskplane_image_tag      = "cbfe9ccd"
+  deskplane_chart_version  = "0.1.0-bdbf7558"
+  deskplane_image_tag      = "bdbf7558"
   deskplane_registry_host  = "registry.gitlab.home.shdr.ch"
   deskplane_registry_user  = var.secrets["gitlab.root_email"]
   deskplane_registry_pass  = var.secrets["gitlab.root_password"]
