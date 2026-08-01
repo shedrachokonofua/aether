75001a588e9a05db133d2a7b2a004295be359eb9
Shedrach Okonofua <shedrachokonofua@gmail.com>
2026-08-01 18:52:49 -0400

    fix(backups): refresh offsite credentials through rclone

--- tofu/aws/offsite-backup.tf
+++ tofu/aws/offsite-backup.tf
@@ -63,8 +63,11 @@ resource "aws_iam_role_policy" "offsite_backup" {
         Effect = "Allow"
         Action = [
           "s3:GetObject",
+          "s3:GetObjectVersion",
           "s3:PutObject",
+          "s3:PutObjectAcl",
           "s3:DeleteObject",
+          "s3:DeleteObjectVersion",
           "s3:GetObjectAttributes",
           "s3:RestoreObject"
         ]
