2e3d33239c1552e5f853f0f35387f0258b0b7fe8
Shedrach Okonofua <shedrachokonofua@gmail.com>
2026-09-04 21:40:04 -0400

    fix: deploy Muse SSE socket timeout repair

--- tofu/home/kubernetes/muse.tf
+++ tofu/home/kubernetes/muse.tf
@@ -6,7 +6,7 @@
 # with an independent random bearer token. There is no PAYG fallback.
 
 locals {
-  muse_image         = "registry.gitlab.home.shdr.ch/so/muse-bridge@sha256:799482e42345a64661d321d3f6a2bab0dbdafaed002a04e8acc443dfde3d587a"
+  muse_image         = "registry.gitlab.home.shdr.ch/so/muse-bridge@sha256:068ac5173acb3a77f7fbf30b94a3dae837cfd1c888a0b044440d987e9510eabf"
   muse_host          = "muse.home.shdr.ch"
   muse_port          = 8080
   muse_ns            = module.namespace["muse"].name
